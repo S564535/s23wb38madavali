544ad51b4d38bbe27d486f882669ffaab43eb205
S564535 <S564535@nwmissouri.edu>
2023-11-10 13:18:30 -0600

    lab 12

--- controllers/Elephant.js
+++ controllers/Elephant.js
@@ -4,9 +4,17 @@ exports.Elephant_list = function(req, res) {
  res.send('NOT IMPLEMENTED: Elephant list');
 };
 // for a specific Elephant.
-exports.Elephant_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Elephant detail: ' + req.params.id);
-};
+exports.Elephant_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Elephant.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+   
 // Handle Elephant create on POST.
 exports.Elephant_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: Elephant create POST');
@@ -70,3 +78,26 @@ exports.Elephant_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     } 
    };
+
+
+
+// Handle Elephant update form on PUT.
+exports.Elephant_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await Elephant.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.Elephant_type) 
+ toUpdate.Elephant_type = req.body.Elephant_type;
+ if(req.body.Elephant_weight) toUpdate.Elephant_weight = req.body.Elephant_weight;
+ if(req.body.Elephant_Size) toUpdate.Elephant_Size = req.body.Elephant_Size;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`);
+ }
+};
