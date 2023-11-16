9b4217484a295feab23fc760b25ec48fb7266dc7
S564535 <S564535@nwmissouri.edu>
2023-11-15 21:47:20 -0600

    bonus

--- routes/Elephant.js
+++ routes/Elephant.js
@@ -1,9 +1,19 @@
 var express = require('express');
 const Elephant_controlers= require('../controllers/Elephant');
 var router = express.Router();
-/* GET Elephants */
+/* GET Elephant */
 router.get('/', Elephant_controlers.Elephant_view_all_Page );
 module.exports = router;
 
 /* GET detail Elephant page */
 router.get('/detail', Elephant_controlers.Elephant_view_one_Page);
+
+/* GET create Elephant page */
+router.get('/create', Elephant_controlers.Elephant_create_Page);
+
+/* GET create update page */
+router.get('/update', Elephant_controlers.Elephant_update_Page);
+
+/* GET delete Elephant page */
+router.get('/delete', Elephant_controlers.Elephant_delete_Page);
+
