f3b169dbc7b5556642c7ec0663faf8e425b168f3
S564535 <S564535@nwmissouri.edu>
2023-11-17 13:13:59 -0600

    added update button and passport

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ElephantRouter = require('./routes/Elephant');
@@ -21,22 +43,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Elephant', ElephantRouter );
+app.use('/Elephant', ElephantRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -49,50 +81,62 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 require('dotenv').config();
-const connectionString = 
-process.env.MONGO_CON
+const connectionString =
+  process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString);
+
+// passport config
+// Use the existing connection
+// The Account model 
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 var Elephant = require("./models/Elephant");
 
 // We can seed the collection if needed on server start
-async function recreateDB(){
- // Delete everything
- await Elephant.deleteMany();
- let instance1 = new 
- Elephant({Elephant_type:"Asian Elephant", Elephant_size:'Large', Elephant_weight:3000});
- instance1.save().then(doc=>{
- console.log("First object saved")}
- ).catch(err=>{
- console.error(err)
- });
+async function recreateDB() {
+  // Delete everything
+  await Elephant.deleteMany();
+  let instance1 = new
+    Elephant({ Elephant_type: "Asian Elephant", Elephant_size: 'Large', Elephant_weight: 3000 });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
- let instance2 = new 
- Elephant({Elephant_type:"African Elephant",Elephant_Size:"Extra Large",Elephant_weight:2000});
- instance2.save().then(doc=>{
- console.log("Second object saved")}
- ).catch(err=>{
- console.error(err)
- });
+  let instance2 = new
+    Elephant({ Elephant_type: "African Elephant", Elephant_Size: "Extra Large", Elephant_weight: 2000 });
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
- let instance3 = new 
- Elephant({Elephant_type:"African Bush Elephant",Elephant_Size:"Extra Large",Elephant_weight:2500});
- instance3.save().then(doc=>{
- console.log("Third object saved")}
- ).catch(err=>{
- console.error(err)
- });
+  let instance3 = new
+    Elephant({ Elephant_type: "African Bush Elephant", Elephant_Size: "Extra Large", Elephant_weight: 2500 });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 }
 
 
 let reseed = true;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 
